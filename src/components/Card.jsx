558131fa596c592ca57d4d13e4c170d86914770a
Matthew <matthewcho399@gmail.com>
2024-04-25 22:35:57 -0700

    catchup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,4 +3,9 @@ import "../styles/card.css";
 export default function Card({ name, url }) {
   return (
     <div className="card-container">
-      <img src={url} alt="ditto" width={23
+      <img src={url} alt="ditto" width={230} height={230} />
+      <hr></hr>
+      <p className="name">{name}</p>
+    </div>
+  );
+}
